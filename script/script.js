2ca29c59b48d9c52076636b1a7b374e80337b567
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2019-12-12 15:36:30 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,34 +1,37 @@
-var listetest = [
-    { "Ecole1" : "EX1", "Lat" : "" },
-    { "Ecole2" : "EX2",  "Lat" : "" },
-    { "Ecole3" : "EX3", "Lat" : "" },
-    { "Ecole4" : "EX4", "Lat" : "" }
-  ];
-
 function SwapOrder(){
     
     function onPositionReceived(){
         PlusVersMoinsProche(position, listelatlon)
         console.log(position);
-        document.getElementById('un').style.order = 4;
-        document.getElementById('deux').style.order = 3;
-        document.getElementById('trois').style.order = 2;
-        document.getElementById('quatre').style.order = 1;
+        var i = 0;
+        for (var i = 0; i < 9; i++) {
+            document.getElementsByClassName(varclasse).style.order = i;
+        }
     }
 
-    function CalculDistance(position, latlon){
+    function CalculDistance(position, latlon){ 
+        /*
+        fonction qui calcul la distance qui sépare deux position.
+        parametres : deux tuples contenant deux int (les latitudes et longitudes)
+        resultat : un float 
+        */
         let latitudeA = position.coords.latitude
         let longitudeA = position.coords.longitude
         let latitudeB = latlon.latitude
         let longitudeB = latlon.longitude
         let x = (longitudeB-longitudeA)*Math.cos((latitudeA+latitudeB)/2)
         let y = latitudeB-latitudeA
-        let z = Math.sqrt((x^2)+(y^2))
+        let z = Math.sqrt((Math.pow(x, 2))+(Math.pow(y, 2)))
         let distance = 1.852*60*z
         return distance
     }
 
-    function PlusVersMoinsProche(position, listelatlon){
+    function PlusVersMoinsProche(position, listelatlon){ 
+        /*
+        retourne la liste des écoles triés de la plus proche de la position donnée à la moins proche
+        parametres :
+        resultat : une liste 
+        */
         let n = 0;
         var res = []
         for (var i = 0; i < length(listelatlon); i++) {
@@ -37,7 +40,8 @@ function SwapOrder(){
         }
     }
 
-    function locationNotReceived(positionError){
+    function locationNotReceived(positionError){  
+        //fonction qui est appelé si la position de l'utilisateur n'a pas pu être récupéré
         console.log(positionError);
     }
 
