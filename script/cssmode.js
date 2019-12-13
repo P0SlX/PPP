b864654044ccb717e49c14225e03566f0ec96cc8
P0SlX <25037788+P0SlX@users.noreply.github.com>
2019-12-13 16:57:20 +0100

    juste un truc

--- script/cssmode.js
+++ script/cssmode.js
@@ -1,9 +1,16 @@
 function ChangeCSS() {
     var checkBox = document.getElementByID("toggle");
-    if (checkBox.checked == true){
-      document.getElementById('pagestyle').setAttribute('href', './style/dark.css');
-    } 
-    else {
+    if (checkBox.checked == true) {
+        document.getElementById('pagestyle').setAttribute('href', './style/dark.css');
+    } else {
         document.getElementById('pagestyle').setAttribute('href', './style/light.css');
     }
-  }
+}
+
+<
+script type = "text/javascript" >
+    document.getElementById("myLink").onclick = function() {
+        document.getElementById("abc").href = "xyz.php";
+        return false;
+    }; <
+/script>
