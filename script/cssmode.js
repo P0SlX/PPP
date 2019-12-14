91529966146409af892c1602a5a715926e28ffda
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2019-12-14 12:42:07 +0100

    Update cssmode.js

--- script/cssmode.js
+++ script/cssmode.js
@@ -3,7 +3,6 @@ function ChangeCSS(){
   var link = document.getElementById('pagestyle');
   if (checkBox.checked == true){
     link.href='./style/dark.css';
-    console.log('oui');
   }
   else { 
       link.href='./style/light.css';
