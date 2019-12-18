64f8a2305a106953fca97ac1047dd1e1a6812888
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2019-12-18 23:50:52 +0100

    GEO
    J'ai suppr le fichier json, j'ai finalement intégré la BD dans le fichier js c'est LARGEMENT plus pratique.
    Sinon le script en lui même est quasi fini, il faut que je debug.

--- script/cssmode.js
+++ script/cssmode.js
@@ -1,10 +1,13 @@
 function ChangeCSS(){
-  var checkBox = document.getElementById('toggle');
-  var link = document.getElementById('pagestyle');
-  if (checkBox.checked == true){
-    link.href='./style/dark.css';
+  /*
+  Cette fonction change le lien href de la page html quand elle est appelée
+  */
+  var checkBox = document.getElementById('toggle'); //le bouton
+  var link = document.getElementById('pagestyle'); //la balise link
+  if (checkBox.checked == true){ //si le bouton est checké
+    link.href='./style/dark.css'; //href prend la page de style './style/dark.css'
   }
-  else { 
-      link.href='./style/light.css';
+  else { //sinon
+      link.href='./style/light.css'; //href prend la page de style './style/light.css'
   }
 }
--- script/geolocation.js
+++ script/geolocation.js
@@ -1,12 +1,151 @@
-var fromJSON = localStorage.getItem("bdlatlon");
-var obj = JSON.parse(fromJSON);
+monJson = {
+    "Polytech Lille": {
+        "lat": 50.607736,
+        "lon": 3.136850
+    },
+    "Polytech Paris-Sud": {
+        "lat": 48.709228,
+        "lon": 2.171136
+    },
+    "Polytech Nancy": {
+        "lat": 48.660120,
+        "lon": 6.188261
+    },
+    "Polytech Orleans": {
+        "lat": 47.844203,
+        "lon": 1.939034
+    },
+    "Polytech Tours": {
+        "lat": 47.363995,
+        "lon": 0.683255
+    },
+    "Polytech Angers": {
+        "lat": 47.480626,
+        "lon": -0.592132
+    },
+    "Polytech Nantes": {
+        "lat": 47.281892,
+        "lon": -1.515896
+    },
+    "Polytech Lyon": {
+        "lat": 45.779316,
+        "lon": 4.868214
+    },
+    "Polytech Clermont-Ferrand": {
+        "lat": 45.760938,
+        "lon": 3.109215
+    },
+    "Polytech Annecy-Chambéry": {
+        "lat": 45.919742,
+        "lon": 6.157915
+    },
+    "Polytech Grenoble": {
+        "lat": 45.184493,
+        "lon": 5.752969
+    },
+    "Polytech Montpellier": {
+        "lat": 43.632789,
+        "lon": 43.632789
+    },
+    "Polytech Nice Sophia": {
+        "lat": 43.615614,
+        "lon": 7.071725
+    },
+    "Polytech Marseille": {
+        "lat": 43.231976,
+        "lon": 5.443125
+    },
+    "Ensimag": {
+        "lat": 45.184638,
+        "lon": 5.752744
+    },
+    "Ecole 42": {
+        "lat": 48.896676,
+        "lon": 2.318507
+    },
+    "Ecole normale supérieure": {
+        "lat": 48.842233,
+        "lon": 2.345181
+    },
+    "Insa Toulouse": {
+        "lat": 43.569683,
+        "lon": 1.467740
+    },
+    "ENSEEIHT": {
+        "lat": 43.602101,
+        "lon": 1.454398
+    },
+    "Centrale Supélec": {
+        "lat": 48.708809,
+        "lon": 2.163995
+    },
+    "Université Pierre et Marie Curie": {
+        "lat": 48.847019,
+        "lon": 2.357370
+    },
+    "Télécom-Paristech": {
+        "lat": 48.713106,
+        "lon": 2.200591
+    },
+    "Isima": {
+        "lat": 45.759327,
+        "lon": 3.111220
+    },
+    "Université Paul Sabatier": {
+        "lat": 43.561984,
+        "lon": 1.470043
+    },
+    "Télécom Nancy": {
+        "lat": 48.669147,
+        "lon": 6.155318
+    },
+    "Epita": {
+        "lat": 48.797981,
+        "lon": 2.357191
+    },
+    "Isae - Supaéro": {
+        "lat": 43.565904,
+        "lon": 1.474605
+    },
+    "Insa Lyon": {
+        "lat": 45.783369,
+        "lon": 4.878059
+    },
+    "ENSIIE": {
+        "lat": 48.626811,
+        "lon": 2.431944
+    },
+    "Université Paris Sud": {
+        "lat": 48.702859,
+        "lon": 2.174252
+    },
+    "Efrei": {
+        "lat": 48.788783,
+        "lon": 2.363658
+    },
+    "Enseirb-Matmeca": {
+        "lat": 44.806569,
+        "lon": -0.605596
+    },
+    "Université Claude Bernard": {
+        "lat": 45.756190,
+        "lon": 4.853846
+    }
+}
+/*
+for (let school in monJson) {
+    let latlon = monJson[school];
+    console.log(school);
+    console.log(latlon);
+}
+*/
 
 function SwapOrder(){
-    function onPositionReceived(position){
-        PlusVersMoinsProche(position, listelatlon)
+    function PositionRecue(position){
+        PlusVersMoinsProche(position)
         console.log(position);
         var i = 0;
-        for (var i = 0; i < 9; i++) {
+        for (let i = 0; i < 9; i++) {
             document.getElementsByClassName(varclasse).style.order = i;
         }
     }
@@ -15,7 +154,7 @@ function SwapOrder(){
         /*
         fonction qui calcul la distance qui sépare deux position.
         parametres : deux tuples contenant deux int (les latitudes et longitudes)
-        resultat : un float 
+        resultat : un float
         */
         let latA = position.coords.latitude
         let lonA = position.coords.longitude
@@ -28,7 +167,7 @@ function SwapOrder(){
         return distance
     }
 
-    function PlusVersMoinsProche(position, listelatlon){ 
+    function PlusVersMoinsProche(position){ 
         /*
         retourne la liste des écoles triés de la plus proche de la position donnée à la moins proche
         parametres :
@@ -36,18 +175,19 @@ function SwapOrder(){
         */
         let n = 0;
         var res = []
-        for (var i = 0; i < length(listelatlon); i++) {
-            res.append(CalculDistance(position, latlon))
-            
+        for (let school in monJson) {
+            let latlon = monJson[school]
+            res.push(CalculDistance(position, latlon))
         }
+        return res
     }
 
-    function locationNotReceived(positionError){  
+    function PoistionNONRecue(positionError){  
         //fonction qui est appelé si la position de l'utilisateur n'a pas pu être récupéré
         console.log(positionError);
     }
 
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(onPositionReceived, locationNotReceived);
+        navigator.geolocation.getCurrentPosition(PositionRecue, PoistionNONRecue);
     }
 };
