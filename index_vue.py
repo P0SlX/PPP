1eee108c0bfc14384cb09c193e0b48ac5b16a17f
P0SlX <25037788+P0SlX@users.noreply.github.com>
2019-12-31 00:30:23 +0100

    Juste un truc

---
+++ index_vue.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# =====================
+# NOM : SAVOURÉ Florian
+# ====================
+from jinja2 import Environment, FileSystemLoader # ne pas modifier
+
+# Ajouter ici les éléments du modèles dont on a besoin
+from Polytech import *
+
+# ne modifiez pas cette fonction !
+def creer_html(fichier_template, fichier_sortie,**infos):
+    """
+    Cette fonction génère automatiquement un fichier à partir d'un
+    template et d'informations
+    paramètres :
+        fichier_template : un fichier template (template HTML par exemple)
+        fichier_sortie : le nom du fichier généré
+        **infos : un nombre indéfini de paramètres qu'il suffit de nommer
+    return : -
+    """
+    env=Environment(loader=FileSystemLoader('.'),trim_blocks=True)
+    template=env.get_template(fichier_template)
+    html=template.render(infos)
+    f = open(fichier_sortie, 'w')
+    f.write(html)
+    f.close()
+
+# Ajouter ici les appels à la fonction creer_html
+
+creer_html("index_template.html", "index.html",
+            nomTemplate = nomDescEcoles(dico_polytech)
+            )
