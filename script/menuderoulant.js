5ae6c67f0c263b08595856b7155b180fe054f983
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2019-12-31 17:17:23 +0100

    débouché

---
+++ script/menuderoulant.js
@@ -0,0 +1,9 @@
+const toggler = document.getElementsByClassName("menu_deroulant");
+let i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".imbrication").classList.toggle("imbrication_active");
+    this.classList.toggle("menu_deroulant_bas");
+  });
+};
