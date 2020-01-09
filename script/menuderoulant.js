487fc328a71bc4140d888d5213a48f182e458d42
Andrew <58080347+PhoqueEberlue@users.noreply.github.com>
2020-01-09 14:55:35 +0100

    ADD back to the top button

--- script/menuderoulant.js
+++ script/menuderoulant.js
@@ -2,8 +2,8 @@ const toggler = document.getElementsByClassName("menu_deroulant");
 let i;
 
 for (i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".imbrication").classList.toggle("imbrication_active");
-    this.classList.toggle("menu_deroulant_bas");
-  });
+  	toggler[i].addEventListener("click", function() {
+    	this.parentElement.querySelector(".imbrication").classList.toggle("imbrication_active");
+    	this.classList.toggle("menu_deroulant_bas");
+  	});
 };
---
+++ script/scrollback.js
@@ -0,0 +1,22 @@
+const ScrollBackBouton = document.getElementById("ScrollBack");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    /*
+    Dès que l'utilisateur va scrol de 80px, le bouton s'affiche.
+    */
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        ScrollBackBouton.style.display = "block";
+    } else {
+        ScrollBackBouton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    /*
+    Quand l'utilisateur clique sur le bouton, cela le renvoie vers le haut de la page.
+    */
+    document.body.scrollTop = 0; // Ligne de code supportée par Safari
+    document.documentElement.scrollTop = 0; // Ligne de code supportée par Chrome, Firefox, IE et Opera
+}
